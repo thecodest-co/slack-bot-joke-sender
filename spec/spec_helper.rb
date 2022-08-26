5a60be33406f0eacc8d9725c90e991310d5a03e1
Damian Wasiluk <49942359+damian0s10@users.noreply.github.com>
2022-08-26 12:27:32 +0200

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require_relative '../src/handler'
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = '.rspec_status'
- 
   config.disable_monkey_patching!
  
   config.expect_with :rspec do |c|
